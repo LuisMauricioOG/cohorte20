163e01df0a6d6acfb9ed527d50ded1466b4216e3
Luis M. Ortiz-Galvez <luismauricio.ortizgz@outlook.com>
2022-12-05 22:08:19 -0600

    Primer práctica sobre revisión del código de otra persona

---
+++ live-coding/live-coding-2/assets/js/app.js
@@ -0,0 +1,26 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location'); //Usar comillas inversas
+//El nombre de las variables no son muy claros, ni tiene comentarios para identificar su función
+
+ //Colocamos el async antes de la definición de la función porque
+async function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+  return response();//No estaba llamando a response
+}
+
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
